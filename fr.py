13e7919caf8e46d7d008c1e466fff68f152d6ea6
ianohurr <ianoheir@email.arizona.edu>
2017-09-28 12:14:23 -0700

    script

--- fr.py
+++ fr.py
@@ -1,30 +1,42 @@
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
 
-    
 def main():
     playerList={}
-    with open('action.txt') as f:
-        for line in f:
-            if ('modified active roster' in line):
-                line=line.split()
-                if(line[0] not in playerList):
-                    playerList[line[0]]={'modified active roster':1}
-                else:
-                    if('modified active roster' not in playerList[line[0]]):
-                        playerList[line[0]]['modified active roster']=1
-                    else:
-                        playerList[line[0]]['modified active roster']+=1
-            elif ('added' in line):
-                line=line.split()
-                if(line[1] not in playerList):
-                    playerList[line[1]]={'Player(s) added':1}
-                else:
-                    if('Player(s) added' not in playerList[line[1]]):
-                        playerList[line[1]]['Player(s) added']=1
-                    else:
-                        playerList[line[1]]['Player(s) added']+=1
+    for line in reversed(list(open('action.txt'))):
+        if ('drafted' in line):
+            line=line.split()
+            if(line[1] not in playerList):
+                playerList[line[1]]={'Players Drafted':0,'Modified active roster':0,'Player(s) added':0,'Player(s) dropped':0,'Player(s) traded':0}
+            playerList[line[1]]['Players Drafted']+=1
+        if ('modified active roster' in line):
+            line=line.split()
+            playerList[line[0]]['Modified active roster']+=1
+        elif ('Add/Drop' in line):
+            line=line.split()
+            playerList[line[1]]['Player(s) added']+=1
+            playerList[line[1]]['Player(s) dropped'] += 1
+        elif ('Add' in line):
+            line = line.split()
+            if(line[0]=='Add' and line[2]=='added'):
+                playerList[line[1]]['Player(s) added'] += 1
+        elif ('Drop' in line):
+            line = line.split()
+            if(line[0]=='Drop' and line[2]=='dropped'):
+                playerList[line[1]]['Player(s) dropped'] += 1
+        elif ('Accepted Trade' in line):
+            line = line.split()
+            playerList[line[0]]['Player(s) traded'] += 1
+            playerList[line[-1]]['Player(s) traded'] += 1
+
                     
-    print(playerList)
-        
+    pretty(playerList)
+
 
 if __name__ == '__main__':
     main()
